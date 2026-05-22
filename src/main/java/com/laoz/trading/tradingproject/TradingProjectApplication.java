d652a20c1bab64a31c7f0cfad505a82db4971576
zwx985 <1163664754@qq.com>
2026-05-22 23:16:09 +0800

    Initial Spring Boot project with MySQL config

---
+++ src/main/java/com/laoz/trading/tradingproject/TradingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.laoz.trading.tradingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laoz/trading/tradingproject/TradingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoz.trading.tradingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
